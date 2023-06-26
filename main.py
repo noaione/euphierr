a0f82b6fe84973393562481ae1ad4b5bfdab4170
noaione <noaione0809@gmail.com>
2023-06-26 16:55:48 +0800

    Bump to 0.5.0

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ if __name__ == "__main__":
 
     skip_time_check = bool(args.skip_time_check)
 
-    logger.info("Starting ArcNCiel/EuphieRR v0.4.0...")
+    logger.info("Starting ArcNCiel/EuphieRR v0.5.0...")
     if LOCK_FILE.exists():
         logger.warning("Lock file exists, exiting")
 
